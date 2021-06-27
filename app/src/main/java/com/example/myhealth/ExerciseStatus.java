ac1e744d7482bb7247509d64cb3070a79d852418
jonghoon.park <hom8181@naver.com>
2021-06-27 22:14:33 +0900

    2021-06-27 Feature stop/restart button add.

---
+++ app/src/main/java/com/example/myhealth/ExerciseStatus.java
@@ -0,0 +1,6 @@
+package com.example.myhealth;
+
+public enum ExerciseStatus {
+    EXERCISE,
+    REST
+}
--- app/src/main/java/com/example/myhealth/MainActivity.java
+++ app/src/main/java/com/example/myhealth/MainActivity.java
@@ -20,14 +20,15 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Chronometer mExerciseStart, mRestTime;
+    private ExerciseStatus currentExerciseStatus;
+
     private Button exerciseStartButton;
     private Button restStartButton;
     private Button stopButton;
+    private Button stopEndButton;
 
     private int exerciseSet = 0;
-    private ScrollView listView;
     private TextView textView;
-    private String gitTest;
 
     ArrayList<Set> items;
 
@@ -39,18 +40,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         exerciseStartButton = findViewById(R.id.exercise_start_btn);
         restStartButton = findViewById(R.id.rest_start_btn);
         stopButton = findViewById(R.id.stop_btn);
+        stopEndButton = findViewById(R.id.stop_end_btn);
         Button resetButton = findViewById(R.id.reset_btn);
 
         mExerciseStart = findViewById(R.id.exercise_time);
         mRestTime = findViewById(R.id.rest_time);
 
-        listView = findViewById(R.id.set_list);
+        ScrollView listView = findViewById(R.id.set_list);
 
         textView = findViewById(R.id.scroll_text);
 
         exerciseStartButton.setOnClickListener(this);
         restStartButton.setOnClickListener(this);
         stopButton.setOnClickListener(this);
+        stopEndButton.setOnClickListener(this);
         resetButton.setOnClickListener(this);
     }
 
@@ -59,13 +62,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.exercise_start_btn:
+                currentExerciseStatus = ExerciseStatus.EXERCISE;
+                if (exerciseSet == 0) {
+                    stopButton.setVisibility(View.VISIBLE);
+                    restStartButton.setText( "1세트 운동 완료 & 휴식 시작");
+                }
+
                 exerciseSet++;
 
                 if (exerciseSet > 1) {
                     String record = "";
 
                     long restTime = (SystemClock.elapsedRealtime() - mRestTime.getBase()) / 1000;
-                    long exerTime = ((SystemClock.elapsedRealtime() - mExerciseStart.getBase() ) / 1000) - restTime;
+                    long exerTime = ((SystemClock.elapsedRealtime() - mExerciseStart.getBase()) / 1000) - restTime;
 
                     Date date = new Date();
                     @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
@@ -76,12 +85,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     record += "             " + (exerciseSet - 1) + "                        "
                             + startTIme + "                       "
-                            + endTIme + "                        "  +   nowTime +   "\n";
+                            + endTIme + "                        " + nowTime + "\n";
                     textView.setText(textView.getText() + "\n" + record);
                     textView.setMovementMethod(new ScrollingMovementMethod());
-                }
 
-                restStartButton.setText(exerciseSet + "세트 운동 완료 & 휴식 시작");
+                    restStartButton.setText(exerciseSet + "세트 운동 완료 & 휴식 시작");
+                }
 
                 mExerciseStart.setBase(SystemClock.elapsedRealtime());
                 mExerciseStart.start();
@@ -91,10 +100,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 exerciseStartButton.setVisibility(View.GONE);
                 restStartButton.setVisibility(View.VISIBLE);
-
-
                 break;
             case R.id.rest_start_btn:
+                currentExerciseStatus = ExerciseStatus.REST;
+
                 if (exerciseSet > 0) {
                     exerciseStartButton.setText("휴식 종료  " + (exerciseSet + 1) + "세트 운동 시작");
                 }
@@ -106,7 +115,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 restStartButton.setVisibility(View.GONE);
                 exerciseStartButton.setVisibility(View.VISIBLE);
+                break;
 
+            case R.id.stop_btn:
+                mExerciseStart.stop();
+                mRestTime.stop();
+                stopButton.setVisibility(View.GONE);
+                stopEndButton.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.stop_end_btn:
+                if (currentExerciseStatus == ExerciseStatus.EXERCISE) {
+                    mExerciseStart.start();
+                }
+                else {
+                    mRestTime.start();
+                }
+                stopButton.setVisibility(View.VISIBLE);
+                stopEndButton.setVisibility(View.GONE);
                 break;
 
             case R.id.reset_btn:
@@ -121,13 +147,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 restStartButton.setVisibility(View.GONE);
                 exerciseStartButton.setVisibility(View.VISIBLE);
+                stopButton.setVisibility(View.GONE);
+                stopEndButton.setVisibility(View.GONE);
                 textView.setText("");
                 break;
-
-            case R.id.stop_btn:
-                mExerciseStart.stop();
-                mRestTime.stop();
-
         }
     }
 
